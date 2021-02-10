b2cba5a0d9241683d0d595bb552dce2a22f66bc6
rohitaggarwal007 <cs17mtech11018@iith.ac.in>
2021-02-10 23:56:05 +0530

    Fix the sanity file.

--- src/test-suite/sanity_check.sh.cmake
+++ src/test-suite/sanity_check.sh.cmake
@@ -26,13 +26,7 @@ fi
 
 SEED_VERSION=$2
 
-if [ ${SEED_VERSION} = "llvm10" ]
-then
-    Absolute_path_of_RepresentationFile=./vocabulary/seedEmbeddingVocab-300-llvm10.txt
-else
-    echo "Please enter llvm10 for seed version"
-    exit
-fi
+Absolute_path_of_RepresentationFile=./vocabulary/seedEmbeddingVocab-300-${SEED_VERSION}.txt
 
 ORIG_FILE=oracle/${EncodingType}_${SEED_VERSION}/ir2vec.txt
 
