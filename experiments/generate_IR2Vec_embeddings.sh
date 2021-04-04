0273c3a9a191751a3a5b04c30bf45443921186fb
Siddharth Jain <jsiddharth1008@gmail.com>
2021-04-04 13:52:05 +0530

    Replaces llvm 10 and clang 10 with llvm 12 and clang 12 respectivly

--- experiments/generate_IR2Vec_embeddings.sh
+++ experiments/generate_IR2Vec_embeddings.sh
@@ -19,7 +19,7 @@ fi
 
 BUILD=$(realpath ${BUILD})
 
-Absolute_path_of_RepresentationFile=$(realpath ../vocabulary/seedEmbeddingVocab-300-llvm10.txt)
+Absolute_path_of_RepresentationFile=$(realpath ../vocabulary/seedEmbeddingVocab-300-llvm12.txt)
 
 TASK_DIR=$1
 if [ -z "${TASK_DIR}" ]; then
@@ -45,10 +45,10 @@ fi
 
 if [ $EncodingType = "SYM" ]; then
 	PASS="sym"
-	Trans_type="${TASK_DIR}_Symbolic_llvm10"
+	Trans_type="${TASK_DIR}_Symbolic_llvm12"
 elif [ $EncodingType = "FA" ]; then
 	PASS="fa"
-	Trans_type="${TASK_DIR}_FlowAware_llvm10"
+	Trans_type="${TASK_DIR}_FlowAware_llvm12"
 else
 	echo "Please enter SYM or FA for EncodingType"
 	exit
