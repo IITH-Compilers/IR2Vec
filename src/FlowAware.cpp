9de49925d3bb4cbad4ba371f0eef60d52066752b
Siddharth Jain <jsiddharth1008@gmail.com>
2021-04-13 01:13:09 +0530

    Type LookUp changes and switch replaced by if-else

--- src/FlowAware.cpp
+++ src/FlowAware.cpp
@@ -593,8 +593,49 @@ void IR2Vec_FA::inst2Vec(
                  i.first->print(outs());
                  outs() << "\n";
                });
+   auto type = I.getType();
+    
+    if(type->isVoidTy()){
+    vec = getValue("voidTy");
+    }
+    else if( type->isFloatingPointTy()){
+    vec = getValue("floatTy");
+    }
+    else if (type->isIntegerTy()){
+	vec= getValue("integerTy");
+    }
+    else if(type->isFunctionTy()){
+    vec= getValue("functionTy");
+    }
+    else if (type->isStructTy()){
+    vec = getValue("structTy");
+    }
+    else if (type->isArrayTy()){
+   vec = getValue("arrayTy");
+    }
+    else if (type->isPointerTy()){
+     vec = getValue("pointerTy");
+    }
+    else if(type->isVectorTy()){
+    vec =getValue("vectorTy");
+    }
+    else if(type->isEmptyTy()){
+    vec =getValue("emptyTy");
+    }
+    else if (type->isLabelTy()){
+    vec = getValue("labelTy");
+    }
+    else if(type->isTokenTy()){
+    vec = getValue("tokenTy");
+    }
+    else if(type->isMetadataTy()){
+    vec = getValue("metadataTy");
+    }
+    else{
+    vec = getValue("unknownTy");
+    }
 
-  switch (I.getType()->getTypeID()) {
+/*  switch (I.getType()->getTypeID()) {
   case 0:
     vec = getValue("voidTy");
     break;
@@ -626,7 +667,7 @@ void IR2Vec_FA::inst2Vec(
     break;
   default:
     vec = getValue("unknownTy");
-  }
+  }*/
   scaleVector(vec, WT);
   std::transform(instVector.begin(), instVector.end(), vec.begin(),
                  instVector.begin(), std::plus<double>());
