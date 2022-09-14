e61f0a32d927b834eaa1dde82bc40a476584aba0
ShikharJain97 <cs22mtech02002@iith.ac.in>
2022-09-14 19:27:34 +0530

    Revert "Added 1)IR2Vec_Wheels 2)Manylinux2014 Compliant Source Code"
    This reverts commit e8b9d92337a67944c2e75267744f7b7eb59a5b35.
    reverting back to "Merge pull request #51 from IITH-Compilers/svkeerthy-patch-1".

--- Manylinux2014_Compliant_Source/pkg/IR2Vec_pkg/IR2Vec_include/version.h.cmake
+++
@@ -1,6 +0,0 @@
-#ifndef VERSION_H
-#define VERSION_H
-
-#define IR2VEC_VERSION "@PROJECT_VERSION@"
-
-#endif // VERSION_H
--- Manylinux2014_Compliant_Source/pkg/test2.sh
+++
@@ -1,7 +0,0 @@
-rm -rf ./build
-rm -rf ./dist
-rm -rf ./IR2Vec_pkg.egg-info
-rm -rf ./IR2Vec_pkg/__pycache__
-
-export LD_LIBRARY_PATH=/home/shikhar_jain/ir2vec_venv/IR2Vec_Python_Package/pkg:$LD_LIBRARY_PATH
-export LIBRARY_PATH=/home/shikhar_jain/ir2vec_venv/IR2Vec_Python_Package/pkg:$LD_LIBRARY_PATH
