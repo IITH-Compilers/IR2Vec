12ba7e76dfc9e8595336e4f2218bb90bd8e8ed06
Shamil <noteness@riseup.net>
2023-07-17 16:11:21 +0530

    Be more verbose in regen-oracle.sh

--- Manylinux2014_Compliant_Source/pkg/regen-oracle.sh
+++ Manylinux2014_Compliant_Source/pkg/regen-oracle.sh
@@ -17,6 +17,7 @@ DEST_FOLDER_FA_P="oracle/FA_${SEED_VERSION}"
 mkdir -p ${DEST_FOLDER_LL}
 
 for d in ${SRC_WD}/*.c ${SRC_WD}/*.cpp ${SRC_WD}/*.cc; do
+	echo "Compiling ${d} to IR"
 	name=$(basename ${d}) && oname=${name%.*} && clang -S -emit-llvm -Xclang -disable-O0-optnone ${d} -o ${DEST_FOLDER_LL}/${oname}.ll &
 done
 wait
@@ -31,6 +32,7 @@ IR2VEC_PATH=../../build/bin/ir2vec
 VOCAB_PATH="../../vocabulary/seedEmbeddingVocab-300-${SEED_VERSION}.txt"
 
 while IFS= read -r d; do
+	echo "Generating embeddings for ${d}"
 	${IR2VEC_PATH} -sym -vocab=${VOCAB_PATH} -o ${DEST_FOLDER_SYM}/ir2vec.txt -level f ${d} &>/dev/null
 	${IR2VEC_PATH} -fa -vocab=${VOCAB_PATH} -o ${DEST_FOLDER_FA}/ir2vec.txt -level f ${d} &>/dev/null
 	${IR2VEC_PATH} -sym -vocab=${VOCAB_PATH} -o ${DEST_FOLDER_SYM_P}/ir2vec.txt -level p ${d} >/dev/null
