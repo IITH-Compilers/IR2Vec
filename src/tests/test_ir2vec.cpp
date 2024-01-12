7be435ba0341a61702fb6f762fbbd1408ac7447d
nishant-sachdeva <nishant.sachdeva@research.iiit.ac.in>
2024-01-12 20:58:56 +0530

    added basic ir2vec testing for all files, using test-class

--- src/tests/test_ir2vec.cpp
+++ src/tests/test_ir2vec.cpp
@@ -18,18 +18,35 @@ const std::string SEED_VERSION = "llvm17";
 const std::string VOCAB_PATH =
     "/home/nishu/nishant/ir2vec/IR2Vec/vocabulary/seedEmbeddingVocab.txt";
 
-void generateAndVerifyEncodings(std::string iname,
-                                IR2Vec::IR2VecMode ir2vecMode, char level,
-                                std::string funcName) {
-  EXPECT_TRUE(ir2vecMode == IR2Vec::FlowAware or
-              ir2vecMode == IR2Vec::Symbolic);
-  EXPECT_TRUE(level == 'p' or level == 'f');
+struct IR2VecConfig {
+  std::string iname;
+  IR2Vec::IR2VecMode ir2vecMode;
+  char level;
+  std::string funcName;
+};
 
+IR2VecConfig getIR2VecConfig(std::string iname, IR2Vec::IR2VecMode ir2vecMode,
+                             char level, std::string funcName) {
+  IR2VecConfig config;
+  config.iname = iname;
+  config.ir2vecMode = ir2vecMode;
+  config.level = level;
+  config.funcName = funcName;
+  return config;
+}
+
+void generateAndVerifyEncodings(IR2VecConfig config) {
+  EXPECT_TRUE(config.ir2vecMode == IR2Vec::FlowAware or
+              config.ir2vecMode == IR2Vec::Symbolic);
+  EXPECT_TRUE(config.level == 'p' or config.level == 'f');
+
+  IR2Vec::iname = config.iname;
   std::unique_ptr<llvm::Module> Module;
   Module = IR2Vec::getLLVMIR();
   std::string vocab_path = VOCAB_PATH;
-  IR2Vec::Embeddings *emb = new IR2Vec::Embeddings(
-      *Module, ir2vecMode, vocab_path, level, nullptr, funcName);
+  IR2Vec::Embeddings *emb =
+      new IR2Vec::Embeddings(*Module, config.ir2vecMode, vocab_path,
+                             config.level, nullptr, config.funcName);
 
   // get the instruction vector map
   llvm::SmallMapVector<const llvm::Instruction *, IR2Vec::Vector, 128>
@@ -57,22 +74,91 @@ void generateAndVerifyEncodings(std::string iname,
   EXPECT_NE(pgmVector.size(), 0);
 }
 
-TEST(FA_F, IR2VecTests) {
-  IR2Vec::iname = "/home/nishu/nishant/ir2vec/IR2Vec/src/test-suite/"
-                  "PE-benchmarks-llfiles-llvm17/channel-assignment.ll";
-  IR2Vec::IR2VecMode ir2vecMode = IR2Vec::FlowAware;
-  char level = 'f';
-  std::string funcName = "";
+/* TODO:: Add tests for other combinations of IR2VecConfig
 
-  generateAndVerifyEncodings(iname, ir2vecMode, level, funcName);
+  fileNames = < readContents of index-llvm17.files >
+
+  TEST_SYM_P
+    for fileName in fileNames:
+      generateAndVerifyEncodings(
+        getIR2VecConfig(fileName, IR2Vec::Symbolic, 'p', "")
+      )
+
+      progVector = getProgVector()
+      verifyProgVectorWithOracle(fileName, progVector)
+
+  TEST_SYM_F
+    for fileName in fileNames:
+      generateAndVerifyEncodings(
+        getIR2VecConfig(fileName, IR2Vec::Symbolic, 'f', "")
+      )
+
+      funcVector = getFuncVector()
+      verifyFuncVectorWithOracle(fileName, funcVector)
+
+  TEST_FA_P
+    for fileName in fileNames:
+      generateAndVerifyEncodings(
+        getIR2VecConfig(fileName, IR2Vec::FlowAware, 'p', "")
+      )
+
+      progVector = getProgVector()
+      verifyProgVectorWithOracle(fileName, progVector)
+
+  TEST_FA_F
+    for fileName in fileNames:
+      generateAndVerifyEncodings(
+        getIR2VecConfig(fileName, IR2Vec::FlowAware, 'f', "")
+      )
+
+      funcVector = getFuncVector()
+      verifyFuncVectorWithOracle(fileName, funcVector)
+*/
+
+class IR2VecTest : public ::testing::Test {
+public:
+  std::string sourceList = "../test-suite/"
+                           "PE-benchmarks-llfiles-llvm17/index-llvm17.files";
+  std::vector<std::string> fileNames = getFileNames(sourceList);
+  std::vector<std::string> getFileNames(std::string sourceList) {
+    std::vector<std::string> fileNames;
+    std::ifstream sourceListFile(sourceList);
+    std::string line;
+    while (std::getline(sourceListFile, line)) {
+      fileNames.push_back(line);
+    }
+    return fileNames;
+  }
+
+  IR2VecTest() {}
+
+  ~IR2VecTest() {}
+};
+
+TEST_F(IR2VecTest, TEST_SYM_P) {
+  for (auto fileName : fileNames) {
+    generateAndVerifyEncodings(
+        getIR2VecConfig(fileName, IR2Vec::Symbolic, 'p', ""));
+  }
 }
 
-TEST(SYM_F, IR2VecTests) {
-  IR2Vec::iname = "/home/nishu/nishant/ir2vec/IR2Vec/src/test-suite/"
-                  "PE-benchmarks-llfiles-llvm17/channel-assignment.ll";
-  IR2Vec::IR2VecMode ir2vecMode = IR2Vec::Symbolic;
-  char level = 'f';
-  std::string funcName = "";
+TEST_F(IR2VecTest, TEST_SYM_F) {
+  for (auto fileName : fileNames) {
+    generateAndVerifyEncodings(
+        getIR2VecConfig(fileName, IR2Vec::Symbolic, 'f', ""));
+  }
+}
 
-  generateAndVerifyEncodings(iname, ir2vecMode, level, funcName);
+TEST_F(IR2VecTest, TEST_FA_P) {
+  for (auto fileName : fileNames) {
+    generateAndVerifyEncodings(
+        getIR2VecConfig(fileName, IR2Vec::FlowAware, 'p', ""));
+  }
+}
+
+TEST_F(IR2VecTest, TEST_FA_F) {
+  for (auto fileName : fileNames) {
+    generateAndVerifyEncodings(
+        getIR2VecConfig(fileName, IR2Vec::FlowAware, 'f', ""));
+  }
 }
