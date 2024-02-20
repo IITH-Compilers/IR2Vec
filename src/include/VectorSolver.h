2e466fecea43da3e034d5543de0f9d31f4bf6910
PrasannaLanka <cs20btech11029@iith.ac.in>
2024-02-20 23:42:04 +0530

    updated format

--- src/include/VectorSolver.h
+++ src/include/VectorSolver.h
@@ -14,40 +14,37 @@ using namespace llvm;
 
 typedef std::vector<std::vector<double>> matrix;
 
-matrix solve(const matrix& A, const matrix& B, int maxIterations = 10000, double tolerance = 1e-8) {
-    int n = A.size();
-    int m = B[0].size();
+matrix solve(const matrix &A, const matrix &B, int maxIterations = 10000,
+             double tolerance = 1e-8) {
+  int n = A.size();
+  int m = B[0].size();
 
-    matrix X(n, std::vector<double>(m, 0.0));
+  matrix X(n, std::vector<double>(m, 0.0));
 
-    for (int k = 0; k < maxIterations; ++k) {
-        double maxDiff = 0.0;
+  for (int k = 0; k < maxIterations; ++k) {
+    double maxDiff = 0.0;
 
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                double sum = B[i][j];
-
-                for (int p = 0; p < n; ++p) {
-                    if (p != i) {
-                        sum -= A[i][p] * X[p][j];
-                    }
-                }
-
-                double oldX = X[i][j];
-                X[i][j] = sum / A[i][i];
-                if(maxDiff < abs(X[i][j] - oldX))
-                {
-                  maxDiff = abs(X[i][j] - oldX);
-                }
-
-            }
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < m; ++j) {
+        double sum = B[i][j];
+        for (int p = 0; p < n; ++p) {
+          if (p != i) {
+            sum -= A[i][p] * X[p][j];
+          }
         }
-
-        if (maxDiff < tolerance) {
-            return X;
+        double oldX = X[i][j];
+        X[i][j] = sum / A[i][i];
+        if (maxDiff < abs(X[i][j] - oldX)) {
+          maxDiff = abs(X[i][j] - oldX);
         }
+      }
     }
 
-    return matrix();
+    if (maxDiff < tolerance) {
+      return X;
+    }
+  }
+
+  return matrix();
 }
 #endif
