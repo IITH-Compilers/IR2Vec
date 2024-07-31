d45c9bf78d4adccba810ae2db79a82ab1ffb8561
svkeerthy <cs17mtech11018@iith.ac.in>
2024-07-31 11:05:43 +0530

    Changing BSD-4 license to Apache 2.0 with LLVM Exceptions

--- seed_embeddings/triplets.sh
+++ seed_embeddings/triplets.sh
@@ -1,8 +1,7 @@
-# Copyright (c) 2021, S. VenkataKeerthy, Rohit Aggarwal
-# Department of Computer Science and Engineering, IIT Hyderabad
+# Part of the IR2Vec Project, under the Apache License v2.0 with LLVM
+# Exceptions. See the LICENSE file for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 #
-# This software is available under the BSD 4-Clause License. Please see LICENSE
-# file in the top-level directory for more details.
 #
 #Usage: bash triplets.sh <build dir> <No of opt> <llFile alist> <output FileNme> <llvm_build>
 
