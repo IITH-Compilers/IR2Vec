6cf732bcbc5dd1b57903fb27d5f178b1a220a8c8
nishant-sachdeva <nishant.sachdeva@research.iiit.ac.in>
2024-10-08 18:51:47 +0530

    BugFix
    SegFault while generating SYM embeddings with sqlite3.ll
    
    Status - Fixed

--- src/Symbolic.cpp
+++ src/Symbolic.cpp
@@ -28,7 +28,7 @@ using namespace IR2Vec;
 using abi::__cxa_demangle;
 
 Vector IR2Vec_Symbolic::getValue(std::string key) {
-  Vector vec;
+  Vector vec(DIM, 0);
   if (vocabulary.find(key) == vocabulary.end())
     IR2VEC_DEBUG(errs() << "cannot find key in map : " << key << "\n");
   else
